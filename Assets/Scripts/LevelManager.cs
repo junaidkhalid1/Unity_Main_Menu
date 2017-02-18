360839c1b1bb486a90094ed117a5883db67e6810
Junaid KHALID <junaidkhalid1@aol.com>
2017-02-18 03:43:52 +0100

    Main Menu Module of PRI Project
    This is one of the module of EPITA Paris PRI project named "Serious
    Game" .

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class LevelManager : MonoBehaviour {
+
+    public Transform mainMenu, optionMenu, playMenu;
+
+    public void LoadScene(string name)
+    {
+        //SceneManager.LoadScene(name);
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit ();
+#endif
+    }
+
+    public void OptionMenu(bool clicked)
+    {
+        if (clicked == true)
+        {
+            optionMenu.gameObject.SetActive(clicked);
+            optionMenu.gameObject.SetActive(true);
+            mainMenu.gameObject.SetActive(false);
+            Debug.Log("I'm pressed in Option if");
+
+        }
+        else
+        {
+            optionMenu.gameObject.SetActive(clicked);
+            optionMenu.gameObject.SetActive(false);
+            mainMenu.gameObject.SetActive(true);
+            Debug.Log("I'm pressed in Option else");
+        }
+    }
+
+    public void PlayMenu(bool clicked)
+    {
+        if (clicked == true)
+        {
+            playMenu.gameObject.SetActive(clicked);
+            playMenu.gameObject.SetActive(true);
+            mainMenu.gameObject.SetActive(false);
+            Debug.Log("I'm pressed in Option if");
+
+        }
+        else
+        {
+            playMenu.gameObject.SetActive(clicked);
+            playMenu.gameObject.SetActive(false);
+            mainMenu.gameObject.SetActive(true);
+            Debug.Log("I'm pressed in Option else");
+        }
+    }
+}
